dab92ce073f1e75ce419e1c31cc8d3bff35c0adf
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2023-10-29 21:53:05 +0000

    Add files via upload

---
+++ i3/polybar/monocle.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#bspc desktop -l tiled || bspc desktop -l monocle 
+# config top_padding 34
+
+
+bspc desktop -l next 
+
+#Needed for arch
+#bspc desktop -f next
+#bspc desktop -f prev
+
