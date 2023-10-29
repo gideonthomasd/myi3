6fc56ae6bc217ed0a3660b9bf345cad6d57a188b
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2023-10-29 21:54:47 +0000

    Add files via upload

---
+++ scripts/powermenu.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#  ____                                                    
+# |  _ \ _____      _____ _ __ _ __ ___   ___ _ __  _   _  
+# | |_) / _ \ \ /\ / / _ \ '__| '_ ` _ \ / _ \ '_ \| | | | 
+# |  __/ (_) \ V  V /  __/ |  | | | | | |  __/ | | | |_| | 
+# |_|   \___/ \_/\_/ \___|_|  |_| |_| |_|\___|_| |_|\__,_| 
+#                                                          
+#  
+# by Stephan Raabe (2023) 
+# ----------------------------------------------------- 
+
+option1="  lock"
+option2="  logout"
+option3="  reboot"
+option4="  power off"
+
+options="$option1\n"
+options="$options$option2\n"
+options="$options$option3\n$option4"
+
+choice=$(echo -e "$options" | rofi -dmenu -config ~/.config/rofi/config-power.rasi -i -no-show-icons -l 4 -width 30 -p "Powermenu") 
+
+case $choice in
+	$option1)
+		#slock ;;
+  		i3lock-fancy ;;
+	$option2)
+		#qtile cmd-obj -o cmd -f shutdown ;;
+  		pkill qtile || pkill i3 ;;
+	$option3)
+		#systemctl reboot ;;
+  		sudo reboot ;;
+	$option4)
+ 		sudo poweroff ;;
+		#systemctl poweroff ;;
+esac
+
